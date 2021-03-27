a3d80865b3555b38533205fb540bb409418d371a
KennethRLarsen <kenla16@student.sdu.dk>
2021-03-27 11:09:00 +0100

    Flocking behavior

---
+++ boid.py
@@ -0,0 +1,70 @@
+from p5 import Vector, stroke, circle
+import numpy as np
+
+class Boid():
+    
+    def __init__(self, x, y, width, height):
+        self.position = Vector(x,y);
+        self.velocity = Vector(*np.random.rand(2)-0.5)*10;
+        self.acceleration = Vector(*np.random.rand(2)-0.5)/2;
+        self.width = width;
+        self.height = height;
+        self.maxSpeed = 5;
+        self.perseption = 100;    # Radius aroind boid
+
+    def align(self, boids):
+        steering = Vector(*np.zeros(2));
+        total = 0;
+        avgVel = Vector(*np.zeros(2));
+        for boid in boids:
+            dist = np.sqrt((boid.velocity.x-self.velocity.x)**2 + (boid.velocity.y-self.velocity.y)**2);
+            if(dist > self.perseption):
+                total +=1;
+                avgVel += boid.velocity;
+        if(total > 0):
+            avgVel.x /= total;
+            avgVel.y /= total;
+            # Get direction of avgVel vector - ie. unitVector
+            avgVecSize = np.sqrt(avgVel.x**2+avgVel.y**2);
+            avgVec.x /= avgVecSize * self.maxSpeed;
+            avgVec.y /= avgVecSize * self.maxSpeed;
+            steering = avgVel - self.velocity;
+            
+        return steering;
+
+    def cohesion():
+        steering = Vector(*np.zeros(2));
+        total = 0;
+        cMass = Vector(*np.zeros(2));
+        for boid in boids:
+
+
+    def separation():
+        j = 0;
+
+    def apply_behaviors(self, boids):
+        alignment = self.align(boids);
+        self.acceleration += alignment;
+
+    def edges(self):
+        if(self.position.x > self.width):
+            self.position.x = 0;
+        elif(self.position.x < 0):
+            self.position.x = self.width;
+        if(self.position.y > self.height):
+            self.position.y = 0;
+        elif(self.position.y < 0):
+            self.position.y = self.height;
+
+    def update(self):
+        self.position += self.velocity;
+        self.velocity += self.acceleration;
+        velVecSize = np.sqrt(self.velocity.x**2 + self.velocity.y**2);
+        if(velVecSize > self.maxSpeed):
+            self.velocity.x = self.velocity.x/velVecSize * self.maxSpeed;
+            self.velocity.y = self.velocity.y/velVecSize * self.maxSpeed;
+        self.edges();
+
+    def show(self):
+        stroke(255);
+        circle((self.position.x, self.position.y), 10);
---
+++ main.py
@@ -0,0 +1,22 @@
+from p5 import setup, draw, size, background, run
+import numpy as np
+from boid import Boid
+
+width = 400;
+height = 200;
+
+# Add 30 boids to list flock
+flock = [Boid(*np.random.rand(2)*1000, width, height) for _ in range(20)]
+
+def setup():
+    size(width, height);
+
+def draw():
+    background(30,30,47);
+
+    for boid in flock:
+        boid.apply_behaviors(flock);
+        boid.update();
+        boid.show();
+
+run();
