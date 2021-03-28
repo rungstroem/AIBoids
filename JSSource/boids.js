4124d5d9578c16f9a0d13180a75b1a5f2a373ec5
Kenneth Rungstroem Larsen <rungstroem@hotmail.com>
2021-03-28 22:37:51 +0200

    Something new

--- JSSource/boids.js
+++ JSSource/boids.js
@@ -16,13 +16,19 @@ class Boid{
 		this.vy = vy;
 		this.ax = ax;
 		this.ay = ay;
-		this.blinkCount = bc;
+		this.blinkCount = bc;	// Problem is with random initialization...
+	}
+	updateBlink(){
+		if(blinkCount > 99){
+			this.blinkCount = 0;
+		}
+		this.blinkCount++;
 	}
 };
 
 function init(){
 	for(let i = 0; i<numBoids; i++){
-		flock.push(new Boid(Math.random()*width, Math.random()*height, Math.random()*1-0.5, Math.random()*1-0.5, Math.random()*0.5-0.25, Math.random()*0.5-0.25), Math.random()*10);
+		flock.push(new Boid(Math.random()*width, Math.random()*height, Math.random()*1-0.5, Math.random()*1-0.5, Math.random()*0.5-0.25, Math.random()*0.5-0.25), Math.floor(Math.random()*101));
 	}
 }
 
@@ -46,12 +52,12 @@ function edges(boid){
 	}
 }
 
-function updateBlink(boid){
+/*function updateBlink(boid){
 	if(boid.blinkCount > 100){
 		boid.blinkCount = 0;
 	}
 	boid.blinkCount++;
-}
+}*/
 
 function updateVel(boid){
 	if(boid.vx > maxVel) boid.vx = maxVel;
@@ -90,6 +96,12 @@ function boidDraw(ctx, boid){
 	ctx.lineWidth = 3;
 	ctx.strokeStyle = "#FF0000";
 	ctx.stroke();
+	if(boid.blinkCount > 90){
+		ctx.fillStyle = "#0000FF";
+	}else{
+		ctx.fillStyle = "#FFFFFF";
+	}
+	ctx.fill();
 	//if(boid.blinkCount > 90){
 	//	ctx.fillStyle = "#0000FF";
 	//}else{
